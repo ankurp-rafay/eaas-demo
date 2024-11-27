a601e222c9662b77524a34e2e64ef61dafa737e6
Ankur <ankur@rafay.co>
2024-11-27 13:27:52 +0000

    pushing output

--- gcp/outputs.tf
+++ gcp/outputs.tf
@@ -2,3 +2,9 @@
 output "public_ip" {
   value = google_compute_instance.example.network_interface[0].access_config[0].nat_ip
 }
+
+
+output "server_port" {
+  value = var.server_port
+  description = "Port on which the server is listening"
+}
