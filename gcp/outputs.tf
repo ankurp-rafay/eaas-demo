e42cbdcd9c01998d89c6d12e3cf2627f93110182
ankurp-rafay <ankur@rafay.co>
2024-11-27 21:28:34 +0530

    Delete gcp directory

--- gcp/outputs.tf
+++
@@ -1,9 +0,0 @@
-# Output variable: Public IP address
-output "public_ip" {
-  value = google_compute_instance.example.network_interface[0].access_config[0].nat_ip
-}
-
-
-output "server_port" {
-  value = var.server_port
-}
